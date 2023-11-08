a6a12db93142a6f48884115a6c49afeba7cb7167
voidklll <716229@student.dusd.net>
2023-11-08 12:45:45 -0800

    11/8/23
    Finished

---
+++ ScriptingProjectP5Ghaly/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        } while (shouldContinue == true);
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
